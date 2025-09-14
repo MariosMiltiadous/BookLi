11375043ded2d1d28428e16c74495b08c33108fe
Marios <46002841+MariosMiltiadous@users.noreply.github.com>
2025-09-15 01:58:24 +0300

    Update library routes

--- src/app/features/library/library-routing.module.ts
+++ src/app/features/library/library-routing.module.ts
@@ -3,25 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { bookResolver } from './resolvers/book.resolver';
 
 const routes: Routes = [
-  {
+ {
     path: 'books',
-    children: [
-      {
-        path: '',
-        loadComponent: () => import('./components/book-list/book-list').then((c) => c.BookList),
-      },
-      {
-        path: 'new',
-        loadComponent: () =>
-          import('./components/book-detail/book-detail').then((c) => c.BookDetail),
-      },
-      {
-        path: ':id',
-        loadComponent: () =>
-          import('./components/book-detail/book-detail').then((c) => c.BookDetail),
-        resolve: { book: bookResolver }, // prefetch before route activates
-      },
-    ],
+    loadComponent: () =>
+      import('./components/book-list/book-list').then(c => c.BookList),
+    title: 'BookLi • Books',
+  },
+  {
+    path: 'books/new',
+    loadComponent: () =>
+      import('./components/book-detail/book-detail').then(c => c.BookDetail),
+    title: 'BookLi • New Book',
+  },
+  {
+    path: 'books/:id',
+    loadComponent: () =>
+      import('./components/book-detail/book-detail').then(c => c.BookDetail),
+    resolve: { book: bookResolver },
+    // runGuardsAndResolvers: 'paramsOrQueryParamsChange', // optional
+    title: 'BookLi • Edit Book',
   },
   { path: '', pathMatch: 'full', redirectTo: 'books' },
 ];
